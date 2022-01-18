e6b2396b2a33135a23d6035b4165b4fec5e3ffd1
Natalia <natakhrom2701@yandex.ru>
2022-01-18 12:48:09 +0300

    add functionality

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync';
+import getUserName from '../src/sli.js';
+
 console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name? ');
+getUserName(userName);
---
+++ src/sli.js
@@ -0,0 +1,5 @@
+const getUserName = (name) => {
+  console.log('Hello, ' + name + '!');
+}
+
+export default getUserName;
