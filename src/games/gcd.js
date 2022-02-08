3ae1595332355ff312e6b6bc763e1bd93b3d156c
Natalia <natakhrom2701@yandex.ru>
2022-02-08 10:41:32 +0300

    fix gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -17,7 +17,7 @@ const getQuestionAndAnswer = () => {
   const num2 = getRandomNumber(1, 50);
 
   const question = `${num1} ${num2}`;
-  const answer = `${greaterCommonDivisor(num1, num2)}`;
+  const answer = String(greaterCommonDivisor(num1, num2));
 
   return [question, answer];
 };
